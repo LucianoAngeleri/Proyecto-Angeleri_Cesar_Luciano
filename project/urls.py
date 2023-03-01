3f777156553e15d605b639e8319e3904c07fe6db
Luciano Angeleri <lcnang@gmail.com>
2023-02-28 23:32:14 -0300

    Añadidas las modificaciones para que funcione la pagina

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from SocialTravel.views import index
 urlpatterns = [
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
 ]
