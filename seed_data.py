27d5ed1b909c4bc474512cedec52e40a51c8d5b5
Luciano Angeleri <lcnang@gmail.com>
2023-03-02 22:42:36 -0300

    Se añadió un seed_data para rellenar varios post

---
+++ seed_data.py
@@ -0,0 +1,25 @@
+from SocialTravel.models import Post
+
+Post(carousel_caption_title ="Un Carousel Title",
+carousel_caption_description ="Un carousel description",
+heading="Mi viaje",
+description="una descripcion"
+).save()
+
+Post(carousel_caption_title ="Un Carousel Title 1",
+carousel_caption_description ="Un carousel description 1",
+heading="Mi viaje 1",
+description="una descripcion 1"
+).save()
+
+Post(carousel_caption_title ="Un Carousel Title 2",
+carousel_caption_description ="Un carousel  2",
+heading="Mi viaje 2",
+description="una descripcion 2"
+).save()
+
+Post(carousel_caption_title ="Un Carousel Title 3",
+carousel_caption_description ="Un carousel description 3",
+heading="Mi viaje 3",
+description="una descripcion 3"
+).save()
